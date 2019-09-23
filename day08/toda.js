afdbbe1e9670dc2f3e45b49f7d072c17f69a2a76
jhjnzed <55371403+jhjnzed@users.noreply.github.com>
2019-09-23 23:22:53 +0800

    Add files via upload

---
+++ day08/toda.js
@@ -0,0 +1,31 @@
+//首先这个斜杠是注释的意思接下来开始写今天的代码
+
+console.log('这里面随便写什么但是一定要记得加引号')
+console.log('console就是一个输出')
+console.log('今晚天气不错真亮')
+
+// 这个是个单行注释 
+/* 这里面随便写什么这个是多行注释*/
+// 还有就是ctrl+/可以注释
+
+var suibian = 123
+
+var name = pikaqiu
+
+var nianling = 18 , xingbie = 难
+
+// var 这个是个定义  后面是变量  然后输出值
+// var 变量 只能是数字字母下划线 而且不能数字开头，虽然可以用中文但不提昌
+
+// var 有驼峰写法  首字母不用大写但是 后面的字母必须大写
+var thisIsGoodsTotalPrice = 100
+  console.log(thisIsGoodsTotalPrice , nianling , xingbie)  
+
+
+
+
+
+
+
+
+
