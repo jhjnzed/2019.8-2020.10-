86112a262248893a39d9fe368a287f019831be09
jhjnzed <107210082@qq.com>
2019-09-26 23:12:20 +0800

    day10

--- js基础day04/5.js
+++ js基础day04/5.js
@@ -1,4 +1,4 @@
-console.log ( NaN == NaN );//？          false     先把他转化为相同类型   每个NaN之前的值都不一定一样
+console.log ( NaN == NaN );//？          false     NaN跟任何都不相等包括自身
 console.log( NaN === NaN ); //？                 false            同上
 console.log ( undefined == null );//？             true    ufd=未定义   null=空     未定义 和空 相似
 console.log ( undefined === null );//？         false       但不是全等的
@@ -6,7 +6,12 @@ console.log ( 1 + "true" ); // ？                 1true    true变成一个字�
 console.log ( 1 + true );  //？                  2     true=1
 console.log ( "abc" > "b" ); //？                false         因为a < b           
 console.log ( "abc" > "aad" ); //？          true       假如 abc=123   abd= 114 
-console.log ( [] == [] ); //？               false     因为每个数组 里面 的 数据都不一定是一样的    
+
+当 复杂类型在 == 时候  就是  false
+复杂类型数据在转布尔值时候就是true
+当等号进行比较时候，如果有一方是布尔值，会把布尔值先转换为数字
+当进行等号比较时候，如果有一方是数字，那么另一方也要转数字
+console.log ( [] == [] ); //？     
 console.log ( [] === [] ); //？                 false     同上
 console.log ( [] == ! [] ); // ？            true    同上
 console.log ( [] == 0 );//？                 true     因为 数组 可能 为0
