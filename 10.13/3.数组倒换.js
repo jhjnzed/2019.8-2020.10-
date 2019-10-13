cdc32eeb4abce1b6a8df389f847df88c0c9f032c
“github’s <1072110082@qq.com>
2019-10-13 21:35:32 +0800

    修改

--- 10.13/3.数组倒换.js
+++ 10.13/3.数组倒换.js
@@ -9,15 +9,15 @@
 
 
 var arr = [11, 32, 55, 47, 79, 23];
+var x= [23, 79, 47, 55, 32, 11];
+var y=0;
 
-for (var min = 0, max = arr.length - 1; min < max; min++, max--){//创建两个变量max，和min，
-    // 相当于数组的第一位和最后一位，
-    // 让第一位和最后一位元素交换，
-    // 交换完成后自增，
-    // 然后第二个和倒数第二个交换，
+y = arr;
+arr = x;
 
-    var temp = arr[min];
-    arr[min] = arr[max];
-    arr[max] = temp;
-}
-console.log(arr)
+x =y ;
+
+console.log(arr ,x)
+
+
+
