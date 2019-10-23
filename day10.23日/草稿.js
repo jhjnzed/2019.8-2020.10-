90e12adba55b37ec34f7794a6e41c137f9378f5a
“github’s <1072110082@qq.com>
2019-10-23 22:27:00 +0800

    10.23小部分作业

---
+++ day10.23日/草稿.js
@@ -0,0 +1,80 @@
+// let peopel = new Object();
+// peopel.name = '憨憨';
+// peopel.age = '18';
+// peopel.hight = 170;
+// peopel.playgame = function () {
+//     console.log('传奇永不熄灭');
+// }
+// console.log(peopel);
+// peopel.playgame()
+
+// for (var key in peopel) {
+//     console.log(key);
+//     console.log(peopel[key]);
+// }
+
+
+// function gaoshiq(name, age, hight) {
+
+//     let peopel = new Object();
+//     peopel.name = name;
+//     peopel.age = age;
+//     peopel.hight = hight;
+//     peopel.playgame = function () {
+//         console.log('传奇永不熄灭');
+//     }
+//     return peopel;
+// }
+
+// gaoshiq('憨憨', 18, 170)
+
+
+
+
+
+
+
+
+
+// var 函数名 = function( 形参 ...){
+//     this. 键=值。。。
+// }
+
+// var 对象名 = new 函数名（实参，。。。）;
+
+// var Peopel = function (name, age, hight, game) {
+//     this.name = name;
+//     this.age = age;
+//     this.hight = hight;
+//     this.game = game;
+// }
+
+// var hanhan = new Peopel('憨憨', '18', '100kg', function () { console.log('无形之刃最为致命') });
+
+
+// console.log(hanhan);
+// hanhan.game();
+
+
+// var a = 3;
+// var b = 4;
+// var c = 5;
+// console.log(a > b && c > b || a < c)
+// parseInt
+
+
+// var sum = 0;
+// for (var i = 300; i <= 400; i++) {
+//     if (i % 3 != 0) {
+//         sum += i;
+
+//     }
+//     if (sum >= 2000) {
+//         break;
+
+//     }
+// }
+// console.log(sum)  
+
+
+
