4437cc8851124e07c97db5b4d7ec9aea255c1365
jhjnzed <1072110082@qq.com>
2020-05-05 22:38:19 +0800

    .

---
+++ 20205月份代码/0.1.hellworld.js
@@ -0,0 +1,22 @@
+// function fn(){
+//     console.log(111)
+// }
+// fn()
+
+// console.log(global)
+
+// setInterval(function(){
+//     console.log('周期')
+// },1000)
+
+
+// setTimeout(function (){
+//     console.log('定时')
+// },2000)
+
+
+
+
+let a=require('./0.2');
+
+console.log(a.age,a.height)
---
+++ 20205月份代码/0.2.js
@@ -0,0 +1,9 @@
+let num = 10;
+let x=20;
+let y=5;
+module.exports = {
+    name: '好饿',
+    age: num*x,
+    height:x/y
+
+}
