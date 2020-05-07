ad750d29565df8ae53bea206a2edd8408a3e6ae1
jhjnzed <1072110082@qq.com>
2020-05-07 20:29:12 +0800

    .

---
+++ 20205月份代码/app.js
@@ -0,0 +1,20 @@
+// const http = require('http');
+// let app = http.createServer();
+// app.on('requst', function (req, res) {
+//     res.write('<h1>hello world</h1>');
+//     res.end();
+// })
+// app.listen(3000, function () {
+//     console.log("请打开http://127.0.0.1:3000")
+// })
+
+
+
+const http = require('http');
+const app = http.createServer();
+app.on('request', (req, res) => {
+    res.end("<h1>guer  EZ</h1>");
+})
+app.listen(80, function () {
+    console.log("请打开 http://127.0.0.1")
+})
