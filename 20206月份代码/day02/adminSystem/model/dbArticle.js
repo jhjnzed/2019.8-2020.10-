e1b44c2c95226414ce886331c42ea98693fc125c
jhjnzed <1072110082@qq.com>
2020-06-10 22:29:16 +0800

    .

--- 20206月份代码/day02/adminSystem/model/dbArticle.js
+++ 20206月份代码/day02/adminSystem/model/dbArticle.js
@@ -1,13 +1,5 @@
 const mongoose = require('mongoose');
 
-// 数据库连接
-// mongoose.connect('mongodb://localhost:27017/db_blog', {
-//   useNewUrlParser: true, useUnifiedTopology: true
-// }).then(() => {
-//   console.log("数据库连接成功");
-// }).catch((err) => {
-//   console.log(err);
-// })
 
 
 let Article = new mongoose.model("Article", new mongoose.Schema({
@@ -42,13 +34,5 @@ let Article = new mongoose.model("Article", new mongoose.Schema({
   }
 }));
 
-// Article.create({
-//   // 标题
-//   title: "我的小羊在睡觉",
-//   // 作者
-//   author: "5ed8c02e1b519a2c2c662e77",
-//   // 内容
-//   content: "山里有个庙,庙里有个老和尚,老和尚给小和尚讲了一故事: 山里的小羊  肖恩在睡觉 "
-// })
 
 module.exports = Article;
